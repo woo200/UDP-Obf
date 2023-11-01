87c5ea457acf2391e9fa06b36979d9f609db18ac
Jack Woo <woo.jack.06@gmail.com>
2023-11-01 08:33:40 -0700

    Added debug opts

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <unistd.h>
+#include <chrono>
 
 #ifdef _WIN64
     #include <winsock2.h>
@@ -77,7 +78,33 @@ void handle_sigint(int s)
     exit(0);
 }
 
-void forward(std::string s_bind_addr, int bind_port, std::string s_remote_addr, int remote_port, int obf)
+void print_bps(int data_lensum)
+{
+    std::string unit = "B";
+    std::string unit2 = "b";
+    float data_len = data_lensum;
+    if (data_len > 1024)
+    {
+        data_len /= 1024;
+        unit = "KiB";
+        unit2 = "Kb";
+    }
+    if (data_len > 1024)
+    {
+        data_len /= 1024;
+        unit = "MiB";
+        unit2 = "Mb";
+    }
+    if (data_len > 1024)
+    {
+        data_len /= 1024;
+        unit = "GiB";
+        unit2 = "Gb";
+    }
+    std::cout << "DEBUG: " << data_len << " " << unit << "/s" << "[ " << data_len * 8 << " " << unit2 << "/s ]" << std::endl;
+}
+
+void forward(std::string s_bind_addr, int bind_port, std::string s_remote_addr, int remote_port, int obf, bool debug)
 {
     // create a socket and listen on udp
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
@@ -99,6 +126,9 @@ void forward(std::string s_bind_addr, int bind_port, std::string s_remote_addr, 
         return;
     }
 
+    if (debug)
+        std::cout << "DEBUG: Socket bound to " << s_bind_addr << ":" << bind_port << std::endl;
+
     // create a buffer to store the data
     char buffer[65536];
 
@@ -109,6 +139,9 @@ void forward(std::string s_bind_addr, int bind_port, std::string s_remote_addr, 
     remote_addr.sin_addr.s_addr = inet_addr(s_remote_addr.c_str());
 
     struct sockaddr_in last_addr;
+
+    auto start_time = std::chrono::high_resolution_clock::now();
+    int data_lensum = 0;
 
     // loop forever
     while (true)
@@ -129,6 +162,19 @@ void forward(std::string s_bind_addr, int bind_port, std::string s_remote_addr, 
             std::cout << "Warning: failed to receive data" << std::endl;
             return;
         }
+        if (debug) {
+            data_lensum += len;
+
+            // check if it has been 5 seconds from start_time
+            auto end_time = std::chrono::high_resolution_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::seconds>(end_time - start_time);
+            if (duration.count() >= 5)
+            {
+                print_bps(data_lensum / 5);
+                start_time = std::chrono::high_resolution_clock::now();
+                data_lensum = 0;
+            }
+        }
 
         // check if sender is remote_addr
         if (sender_addr.sin_addr.s_addr == remote_addr.sin_addr.s_addr && sender_addr.sin_port == remote_addr.sin_port)
@@ -148,11 +194,12 @@ int main(int argc, char** argv)
 {
     MODE mode = CLIENT;
 
+    bool debug = false;
     std::string bind_addr, remote_addr;
     int bind_port, remote_port;
     int c;
 
-    while ((c = getopt(argc, argv, "csh")) != -1)
+    while ((c = getopt(argc, argv, "cshd")) != -1)
     {
         switch(c)
         {
@@ -162,6 +209,9 @@ int main(int argc, char** argv)
             case 's':
                 mode = SERVER;
                 break;
+            case 'd':
+                debug = true;
+                continue;
             case 'h':
             default:
                 print_usage(argv[0]);
@@ -215,14 +265,20 @@ int main(int argc, char** argv)
     #ifdef _WIN64
         WSADATA wsaData;
         WSAStartup(MAKEWORD(2, 2), &wsaData);
+        if (debug)
+            std::cout << "DEBUG: WSAStartup" << std::endl;
     #endif
 
     switch (mode) {
         case CLIENT:
-            forward(bind_addr, bind_port, remote_addr, remote_port, -1);
+            if (debug)
+                std::cout << "DEBUG: Starting from CLIENT mode" << std::endl;
+            forward(bind_addr, bind_port, remote_addr, remote_port, -1, debug);
             break;
         case SERVER:
-            forward(bind_addr, bind_port, remote_addr, remote_port, 1);
+            if (debug)
+                std::cout << "DEBUG: Starting from SERVER mode" << std::endl;
+            forward(bind_addr, bind_port, remote_addr, remote_port, 1, debug);
             break;
     }
     
